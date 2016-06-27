c620f900bbc6e843b093b8c3ef1fa8a838016cf3
Stephanie Nemeth <traumverloren@gmail.com>
2016-06-27 22:21:09 +0200

    add styles css, start working on prompt

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from '../styles'
 
 const Home = React.createClass({
   render () {
     return (
-      <div>Hello from Home Component!</div>
+      <div className="container-fluid text-center" style={styles.homeBg}>
+          <div className="jumbotron col-xs-8 text-center" style={styles.jumbotron}>
+            <h1 style={{color: '#000000'}}>Search Wikipedia!</h1>
+            <div>
+            // PromptContainer w/ biz logic
+              // Prompt function with form and onSubmit action variable
+            </div>
+          </div>
+        </div>
     )
   }
 });
---
+++ app/styles/index.js
@@ -0,0 +1,20 @@
+const styles = {
+  homeBg: {
+    backgroundImage: 'url("../app/images/102H.jpg")',
+    backgroundSize: 'cover',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+    minHeight: '100%'
+  },
+
+  jumbotron: {
+    background: '#fff',
+    fontFamily: 'Courier',
+    paddingBottom: '25px'
+  },
+};
+
+export default styles
