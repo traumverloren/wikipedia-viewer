4c948b53d1f1beb3f5eece891d9502c4a4c1ab57
Stephanie Nemeth <traumverloren@gmail.com>
2016-06-30 21:12:35 +0200

    update gitignore and type for form to be more doge-ish

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -5,27 +5,27 @@ const PromptContainer = React.createClass({
   render () {
     return (
       <div className="jumbotron col-sm-6 text-center" style={styles.jumbotron}>
-        <h3 style={{color: '#000000'}}>Search Wikipedia!</h3>
+        <h3 style={{color: '#000000'}}>Such Wikipedia</h3>
         <div className="col-sm-12">
           <form>
             <div className="form-group">
               <input
                 className="form-control"
-                placeholder="I'm searching for..."
+                placeholder="So Wow I Seek"
                 type="text" />
             </div>
             <div className="form-group col-sm-6">
               <button
                 className="btn btn-success"
                 type="submit">
-                Search
+                Much Search
               </button>
             </div>
             <div className="form-group col-sm-offset-6">
               <button
                 className="btn btn-warning"
                 type="submit">
-                Random Me!
+                Amaze Random
               </button>
             </div>
           </form>
