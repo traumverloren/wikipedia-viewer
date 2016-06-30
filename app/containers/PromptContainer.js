43c17c6b3ff00e73b6056d707a80c69ba3cb27bf
Stephanie Nemeth <traumverloren@gmail.com>
2016-06-30 22:29:01 +0200

    WIP on getting prompt to handle data

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -9,16 +9,43 @@ class PromptContainer extends Component {
     }
   }
 
+  onUpdateQuery (event) {
+    this.setState({
+      keyword: event.target.value
+    });
+  }
+
+  onSubmitQuery (event) {
+    event.preventDefault();
+    const { keyword } = this.state
+    this.setState({
+      keyword: ''
+    });
+
+    this.context.router.push({
+      pathname: '/results',
+      query: {
+        // destructure this param into a concise object.  take away key if same is same as param.
+        keyword
+      }
+    });
+  }
+
+  // The bind(this) will go away once move this to Prompt component...
+
   render () {
     return (
       <div className="jumbotron col-sm-6 text-center" style={styles.jumbotron}>
         <h3 style={{color: '#000000'}}>Such Wikipedia</h3>
         <div className="col-sm-12">
-          <form>
+          <form onSubmit={this.onSubmitQuery.bind(this)}>
             <div className="form-group">
               <input
                 className="form-control"
                 placeholder="So Wow, I Find"
+
+                onChange={this.onUpdateQuery.bind(this)}
+                value={this.state.keyword}
                 type="text" />
             </div>
             <div className="form-group col-sm-6">
