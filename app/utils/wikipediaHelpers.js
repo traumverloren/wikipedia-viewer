df5fabdf26169926a3814308968c14bc989f895b
Stephanie Nemeth <traumverloren@gmail.com>
2016-07-03 15:42:09 +0200

    add default keyword of doge if no search term submitted

--- app/utils/wikipediaHelpers.js
+++ app/utils/wikipediaHelpers.js
@@ -1,6 +1,9 @@
 import jsonp from 'jsonp-promise'
 
 export async function getResults(keyword) {
+  if (keyword === '') {
+    keyword = 'doge'
+  }
   try {
     const results = await jsonp(`https://en.wikipedia.org/w/api.php?format=json&action=opensearch&limit=5&search=${keyword}`).promise;
     return results
